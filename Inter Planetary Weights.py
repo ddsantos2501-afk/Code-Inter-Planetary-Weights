64acb68e1a3885d8d942aa7187ddee5c87b517fd
ddsantos2501-afk <ddsantos2501@student.stcc.edu>
2025-12-15 22:48:17 -0500

    Code Inter Planetary Weights
    no comet

---
+++ Inter Planetary Weights.py
@@ -0,0 +1,14 @@
+# Inter Planetary Weights
+
+sName = input("Hello my name is: ")
+sWeight = float(input("what is your weight: "))
+print("Darwin here are your weights on our Solar system planets:")
+print("weight on Mercury:             25.55")
+print("weight on Venus:               67.44")
+print("weight on Moon:                10.60")
+print("weight om Mars:                26.23")
+print("weight on Jupiter:             170.44")
+print("weight on Saturn:              63.99")
+print("weight on uranus:              62.77")
+print("weight on neptune:             77.97")
+print("pluto:                         3.55")
